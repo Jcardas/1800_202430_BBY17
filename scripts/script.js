2511e7f289ae9b9dfaeacab467d00efb31a6336e
Felix Nguyen <pnguyen72@my.bcit.ca>
2024-11-05 21:04:25 -0800

    Fix commit 54c55ed225f67cb6ff9c9454372fa1deb4392ff3
    only redirect logged in users to main if they try to access index.html

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@ $("#footer-container").load("./skeleton/footer.html");
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     $("#header-container").load("./skeleton/header_after_login.html");
-    gotoURL("/main.html");
+    if (window.location.pathname == "/index.html") {
+      gotoURL("/main.html");
+    }
   } else {
     $("#header-container").load("./skeleton/header_before_login.html");
     if (window.location.pathname != "/login.html") {
