72e7b8936ac10587e8c5702e9a553a93d5a72f33
Justin Cardas <justincardas@gmail.com>
2024-11-06 17:53:52 -0800

    Initial Messaging Functionality and User Greeting

---
+++ scripts/messages.js
@@ -0,0 +1,38 @@
+// Select input and send button
+const messageInput = document.querySelector('.message-input');
+const sendButton = document.querySelector('.send-button');
+
+// References the database collection for messages.
+const messagesRef = db.collection('messages');
+
+function sendMessage() {
+  const messageText = messageInput.value.trim();
+
+  // if there is text in the field, append it with the timestamp to the database.
+  if (messageText) {
+    messagesRef.add({
+      text: messageText,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+    //Log the messsage being added in the console.
+    .then(() => {
+      console.log("Message '" + messageText + "' added to database");
+      messageInput.value = '';
+    })
+    // Catch any errors.
+    .catch((error) => {
+      console.error("Error adding message: ", error);
+    });
+  }
+}
+
+//Functionality for the send button.
+sendButton.addEventListener('click', sendMessage);
+
+//Functionality for pressing enter to send a message.
+messageInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    sendMessage();
+  }
+});
