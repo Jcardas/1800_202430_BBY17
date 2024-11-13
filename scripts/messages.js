20b3ec80269831e256beb8efbd5dea25f595c211
Felix Nguyen <pnguyen72@my.bcit.ca>
2024-11-12 23:35:09 -0800

    First attempt of the chat feature
    no live update

--- scripts/messages.js
+++ scripts/messages.js
@@ -1,30 +1,133 @@
+var currentUser;
+var senderAvatar;
+var recipientAvatar;
+var messagePage = document.querySelector(".msg-page");
+var messageBottom = document.querySelector(".msg-bottom");
+
+firebase.auth().onAuthStateChanged((user) => {
+  currentUser = user;
+  db.collection("users")
+    .doc(user.uid)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) return;
+      data = doc.data();
+      if (data.avatar) {
+        senderAvatar = data.avatar;
+      }
+    });
+});
+
+var chatDoc;
+const recipientForm = document.querySelector("form.username");
+recipientForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const recipientInput = recipientForm.querySelector("input");
+  const recipientPfp = recipientForm.querySelector("img");
+
+  const recipientID = recipientInput.value.trim();
+  db.collection("users")
+    .doc(recipientID)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        alert("User ID does not exist");
+        return;
+      }
+      data = doc.data();
+
+      // Set recipient profile pic
+      if (data.avatar) {
+        recipientPfp.src = recipientAvatar = data.avatar;
+      }
+
+      // Set recipient user name
+      const recipientName = document.createElement("p");
+      recipientName.innerText = data.name;
+      recipientInput.replaceWith(recipientName);
+
+      // Populate with existing messages (if exist)
+      // the chatID might be "<our ID>+<their ID> or <their ID>+<our ID>"
+      // (depending on who initiated the chat first)
+      // so try both ids to see what work
+      messagePage.innerHTML = "";
+      chatDoc = db
+        .collection("messages")
+        .doc(`${recipientID}+${currentUser.uid}`);
+      chatDoc.get().then((doc) => {
+        if (!doc.exists) {
+          chatDoc = db
+            .collection("messages")
+            .doc(`${currentUser.uid}+${recipientID}`);
+          chatDoc.get().then((doc) => {
+            if (!doc.exists) {
+              return;
+            }
+            data = doc.data();
+            data.messages.map(addMessage);
+          });
+          return;
+        }
+        data = doc.data();
+        data.messages.map(addMessage);
+      });
+
+      // Unhide the text input
+      messageBottom.style.display = "";
+    });
+});
+
 // Select input and send button
 const messageInput = document.querySelector(".message-input");
 const sendButton = document.querySelector(".send-button");
 
-// References the database collection for messages.
-const messagesRef = db.collection("messages");
-
 function sendMessage() {
   const messageText = messageInput.value.trim();
+  if (!messageText) return;
 
-  // if there is text in the field, append it with the timestamp to the database.
-  if (messageText) {
-    messagesRef
-      .add({
-        text: messageText,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      })
-      //Log the messsage being added in the console.
-      .then(() => {
-        console.log("Message '" + messageText + "' added to database");
-        messageInput.value = "";
-      })
-      // Catch any errors.
-      .catch((error) => {
-        console.error("Error adding message: ", error);
-      });
+  const message = {
+    from: currentUser.uid,
+    text: messageText,
+    timestamp: Date.now(), // not used for now
+  };
+  chatDoc
+    .set(
+      {
+        messages: firebase.firestore.FieldValue.arrayUnion(message),
+      },
+      { merge: true }
+    )
+    .then(() => addMessage(message))
+    .catch(console.error);
+  messageInput.value = "";
+}
+
+function addMessage(message) {
+  let type;
+  let avatar;
+  if (message.from == currentUser.uid) {
+    type = "outgoing-message";
+    avatar = senderAvatar;
+  } else {
+    type = "incoming-message";
+    avatar = recipientAvatar;
   }
+  if (!avatar) {
+    avatar = "./assets/profile_photo.png";
+  }
+
+  document.querySelector(".msg-page").insertAdjacentHTML(
+    "beforeend",
+    `
+    <div class=${type}>
+      <p>${message.text}</p>
+      <img
+        class="m_userprofile"
+        src=${avatar}
+        alt="profile-photo"
+      />
+    </div>`
+  );
 }
 
 //Functionality for the send button.
