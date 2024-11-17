d390d54318cb41fd8a371955a6596d815d7b72e3
Justin Cardas <justincardas@gmail.com>
2024-11-16 16:54:46 -0800

    Listing Styling and basic clickability

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,3 @@
-insertNameFromFirestore();
-
-function insertNameFromFirestore() {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      console.log(`${user.uid} is logged in.`);
-      currentUser = db.collection("users").doc(user.uid);
-      currentUser.get().then((userDoc) => {
-        if (!userDoc.exists) return;
-        let userName = userDoc.data().name;
-        console.log(userName);
-        document.getElementById("name-goes-here").innerText = userName;
-      });
-    } else {
-      console.log("No user is logged in.");
-    }
-  });
-}
-
 displayCardsDynamically("products"); //input param is the name of the collection
 
 function displayCardsDynamically(collection) {
