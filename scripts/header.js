b9431d7a1ca98312fa53c46e9c3613ceeb780810
Justin Cardas <justincardas@gmail.com>
2024-11-19 10:52:21 -0800

    Tab Selector and google search

--- scripts/header.js
+++ scripts/header.js
@@ -18,3 +18,29 @@ darkModeToggle.addEventListener("input", () => {
   darkModeLink.disabled = !darkModeToggle.checked;
   localStorage.setItem("dark-mode", !darkModeLink.disabled);
 });
+
+
+var currentPage = window.location.pathname;
+currentPage = currentPage.replace("/", "").replace(".html", "");
+
+console.log(currentPage);
+
+var selectedTab = document.getElementById(currentPage);
+if (selectedTab) {
+  console.log(selectedTab);
+
+  selectedTab.classList.add("selected-tab");
+}
+
+const f = document.getElementById("searchbar-form");
+const q = document.getElementById("navbar-searchbar");
+const google = "https://www.google.com/search?q=";
+
+function submitted(event) {
+  event.preventDefault();
+  const url = google + q.value;
+  const win = window.open(url, "_blank");
+  win.focus();
+}
+
+f.addEventListener("submit", submitted);
