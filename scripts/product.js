999d46afa9707d5a5cef7b9ab36c0a65d78bdc4c
Felix Nguyen <pnguyen72@my.bcit.ca>
2024-12-04 00:29:29 -0800

    Incorporate review to product page instead of a separate page

--- scripts/product.js
+++ scripts/product.js
@@ -1,12 +1,15 @@
 const productContainer = document.getElementById("product-content-container");
+const reviewBtn = productContainer.querySelector(".review-btn");
 const messageButton = document.getElementById("message-button");
 
 var sellerID;
+var listingID;
+var isReviewing = false;
 retrieveProductData().then(retrieveSellerData);
 
 function retrieveProductData() {
   const urlParams = new URLSearchParams(window.location.search);
-  const listingID = urlParams.get("id");
+  listingID = urlParams.get("id");
 
   return db
     .collection("listings")
@@ -19,23 +22,43 @@ function retrieveProductData() {
       const price = data.price;
       const photo = data.fileURL;
       const description = data.description;
+      const rating = data.averageRating;
       sellerID = data.userID;
 
       productContainer.querySelector("#product-photo").src = photo;
       productContainer.querySelector("#price").innerText = `\$${price}${unit}`;
       productContainer.querySelector("#product-name").innerText = name;
       productContainer.querySelector("#product-desc").innerText = description;
+      setRating(rating);
 
       getCurrentUser().then((user) => {
         if (user.uid == sellerID) {
           productContainer.querySelector("#edit-post").style.display = "";
+        } else {
+          productContainer.querySelector(".review-btn").style.display = "";
         }
       });
     });
 }
 
+var currentRating;
+function setRating(rating) {
+  currentRating = rating;
+  stars.forEach((star) => (star.style.color = ""));
+  for (let i = 1; i <= rating; ++i) {
+    productContainer.querySelector(`.star${i}`).innerText = "star";
+  }
+  const nextStar = productContainer.querySelector(`.star${Math.ceil(rating)}`);
+  if (rating % 1 > 0.75) {
+    nextStar.innerText = "star";
+  } else if (rating % 1 > 0.25) {
+    nextStar.innerText = "star_half";
+  }
+}
+
 function retrieveSellerData() {
-  db.collection("users")
+  return db
+    .collection("users")
     .doc(sellerID)
     .get()
     .then((doc) => {
@@ -58,15 +81,75 @@ messageButton.addEventListener("click", () => {
   });
 });
 
-function reviewProduct() {
-  let params = new URL(window.location.href);
-  let ID = params.searchParams.get("id");
-  localStorage.setItem("listingDocID", ID);
-  window.location.href = "review.html";
-}
+reviewBtn.onclick = function () {
+  if (isReviewing) {
+    isReviewing = false;
+    reviewBtn.innerText = "Review";
+    let rating = 0;
+    stars.forEach((star) => {
+      if (star.textContent === "star") {
+        rating++;
+      }
+    });
+    if (rating > 0) {
+      postRating(rating)
+        .then(getAverageRating)
+        .then(postAverageRating)
+        .then(setRating);
+    } else {
+      setRating(currentRating);
+    }
+  } else {
+    isReviewing = true;
+    reviewBtn.innerText = "Cancel";
+    stars.forEach((star) => {
+      star.innerText = "star_outline";
+      star.style.color = "#e5e5e5";
+    });
+  }
+};
 
 function editPost() {
   const params = new URL(window.location.href);
   const id = params.searchParams.get("id");
   window.location.assign(`listing_info.html?edit=${id}`);
+}
+
+const stars = productContainer.querySelectorAll(".star");
+stars.forEach(
+  (star, index) =>
+    (star.onclick = () => {
+      if (!isReviewing) return;
+      reviewBtn.innerText = "Submit";
+      for (let i = 0; i <= index; i++) {
+        stars[i].innerText = "star";
+      }
+      for (let i = index + 1; i < 5; i++) {
+        stars[i].innerText = "star_outline";
+      }
+    })
+);
+
+function postRating(rating) {
+  return getCurrentUser().then((user) => {
+    const data = {};
+    data[`ratings.${user.uid}`] = rating;
+    db.collection("listings").doc(listingID).update(data);
+  });
+}
+
+function getAverageRating() {
+  return db
+    .collection("listings")
+    .doc(listingID)
+    .get()
+    .then((doc) => sum(Object.values(doc.data().ratings)));
+}
+
+function postAverageRating(rating) {
+  return db
+    .collection("listings")
+    .doc(listingID)
+    .update({ averageRating: rating })
+    .then(() => rating);
 }
