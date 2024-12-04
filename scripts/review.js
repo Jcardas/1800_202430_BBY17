8fcad44faa9b2ba9d6eff09b0cb1c3b5a287ba3d
Felix Nguyen <pnguyen72@my.bcit.ca>
2024-12-03 22:20:46 -0800

    Simplify and rework review page
    only star rating, no time for other things

--- scripts/review.js
+++ scripts/review.js
@@ -1,5 +1,4 @@
 var listingDocID = localStorage.getItem("listingDocID");
-console.log("Retrieved listing ID:", listingDocID);
 
 function getListingName(id) {
   db.collection("listings")
@@ -14,52 +13,56 @@ function getListingName(id) {
 getListingName(listingDocID);
 
 const stars = document.querySelectorAll(".star");
-
 stars.forEach((star, index) => {
-  star.addEventListener("click", () => {
-    for (let i = 0; i <= index; i++) {
-      document.getElementById(`star${i + 1}`).textContent = "star";
-    }
-  });
+  star.onmouseover = () => rate(index + 1);
+  star.onclick = () => rate(index + 1);
 });
 
-function writeReview() {
-  console.log("inside write review");
-  let listingTitle = document.getElementById("review-title").value;
-  let listingDescription = document.getElementById("review-description").value;
-  let fresh = document.querySelector('input[name="fresh"]:checked').value;
+function rate(rating) {
+  for (let i = 1; i <= rating; i++) {
+    document.getElementById(`star${i}`).textContent = "star";
+  }
+  for (let i = rating + 1; i <= 5; i++) {
+    document.getElementById(`star${i}`).textContent = "star_outline";
+  }
+}
 
-  const stars = document.querySelectorAll(".star");
-  let listingRating = 0;
+function writeReview() {
+  let rating = 0;
   stars.forEach((star) => {
     if (star.textContent === "star") {
-      listingRating++;
+      rating++;
     }
   });
 
-  console.log(listingTitle, listingDescription, fresh, listingRating);
+  postRating(rating)
+    .then(getAverageRating)
+    .then(postAverageRating)
+    .then(() => {
+      alert("Review has been posted");
+      window.location.assign(`each_product.html?id=${listingDocID}`);
+    });
+}
 
-  var user = firebase.auth().currentUser;
-  if (user) {
-    var currentUser = db.collection("users").doc(user.uid);
-    var userID = user.uid;
+function postRating(rating) {
+  return getCurrentUser().then((user) => {
+    const data = {};
+    data[`ratings.${user.uid}`] = rating;
+    db.collection("listings").doc(listingDocID).update(data);
+  });
+}
 
-    db.collection("reviews")
-      .add({
-        listingDocID: listingDocID,
-        userID: userID,
-        title: listingTitle,
-        description: listingDescription,
-        fresh: fresh,
-        rating: listingRating,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
-      })
-      .then(() => {
-        alert("Review has been posted");
-        window.location.href = "main.html";
-      });
-  } else {
-    console.log("No user is signed in");
-    window.location.href = "review.html";
-  }
+function getAverageRating() {
+  return db
+    .collection("listings")
+    .doc(listingDocID)
+    .get()
+    .then((doc) => sum(Object.values(doc.data().ratings)));
+}
+
+function postAverageRating(rating) {
+  return db
+    .collection("listings")
+    .doc(listingDocID)
+    .update({ averageRating: rating });
 }
